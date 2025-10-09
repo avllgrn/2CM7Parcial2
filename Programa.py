7142c15ab16314e0a4c867882aacec9059d6d09e
avllgrn <avillagran@ipn.mx>
2025-10-09 08:09:03 -0600

    Lista con n ceros

--- Programa.py
+++ Programa.py
@@ -3,10 +3,9 @@ from os import system
 if __name__ == '__main__':
     system('cls')
 
-    L = list()
-
     n = int(input('Cuántos datos? '))
 
+    L = []
     for i in range(n):
         print(f'L {L} con {len(L)} dato(s).')
         L.append(0)
