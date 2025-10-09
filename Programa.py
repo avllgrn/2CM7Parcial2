b1c9ccd5e347948573aa1d2693a6b87234e2b6ee
avllgrn <avillagran@ipn.mx>
2025-10-09 08:14:08 -0600

    Lista con n cadenas ingresadas

--- Programa.py
+++ Programa.py
@@ -5,7 +5,11 @@ if __name__ == '__main__':
 
     n = int(input('Cuántos datos? '))
 
-    L = [0 for i in range(n)]
+    L = []
+    for i in range(n):
+        print(f'L {L} con {len(L)} dato(s).')
+        cadena = input(f'Ingresa cadena {i} ')
+        L.append(cadena)
     
     print()
     print(f'L {L} con {len(L)} dato(s).')
