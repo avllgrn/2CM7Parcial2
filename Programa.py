ca09aae56875415680ce86fa66e8d4794a35fbdb
avllgrn <avillagran@ipn.mx>
2025-10-22 08:21:28 -0600

    Lista con ceros

--- Programa.py
+++ Programa.py
@@ -1,23 +1,23 @@
 from os import system
 
+def generaListaCeros(n):
+    lista = []   # lista vacia
+    for i in range(n):
+        lista.append(0)
+    return lista
+
+def muestraLista(L):
+    n = len(L)
+    for i in range(n):
+        print(f'[{i}] = {L[i]}')
+
 if __name__ == '__main__':
     system('cls')
 
-    listaInt = [11, 22, 33, 44, 55]
-    listaFloat = [1.1, 2.2, 3.3, 4.4, 5.5]
-    listaStr = ['uno', 'dos', 'tres', 'cuatro', 'cinco']
-    
-    print(f'   listaInt = {listaInt}')
-    for dato in listaInt:
-        print(f'{dato}')
-    print()
+    n = int(input('¿Cuántos datos? '))
 
-    print(f'   listaFloat = {listaFloat}')
-    for dato in listaFloat:
-        print(f'{dato}')
-    print()
+    ceros = generaListaCeros(n)
 
-    print(f'   listaStr = {listaStr}')
-    for dato in listaStr:
-        print(f'{dato}')
+    print(f'ceros = {ceros}')
+    muestraLista(ceros)
     print()
