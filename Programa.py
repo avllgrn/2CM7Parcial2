e1ce12112868c4acab942840008c7548bc1ecb08
avllgrn <avillagran@ipn.mx>
2025-10-23 07:28:16 -0600

    Suma y resta de matrices

--- Programa.py
+++ Programa.py
@@ -10,49 +10,91 @@ def muestraMatriz(M):
             print(M[i][j], end='\t')
         print()
 
-def generaConRandrange(m, n):
+def generaInicializada(m, n):
     M = []
     for i in range(m):
         renglon = []
         for j in range(n):
-            valor = randrange(101)#/10
-            renglon.append( valor )
+            renglon.append(0)
         M.append(renglon)
         # print(M)
     return M
 
-def generaTranspuesta(M):
-    MT = []
+def generaConRandrange(m, n, ini, fin):
+    M = []
 
-    m = len(M)
-    n = len(M[0])
-
-    mT = n
-    nT = m
-    for i in range(mT):
-        renglon = []
-        for j in range(nT):
-            renglon.append(0)
-        MT.append(renglon)
+    if ini>fin:
+        aux = ini
+        ini=fin
+        fin=aux
 
     for i in range(m):
+        renglon = []
         for j in range(n):
-            MT[j][i] = M[i][j]
-    return MT   
+            valor = randrange(ini, fin)
+            renglon.append( valor )
+        M.append(renglon)
+        # print(M)
+
+    return M
+
+def suma(A, B):
+    if len(A)==len(B) and len(A[0])==len(B[0]):
+        m = len(A)
+        n = len(B[0])
+
+        C = generaInicializada(m, n)
+        for i in range(m):
+            for j in range(n):
+                C[i][j] = A[i][j]+B[i][j]
+        return C
+    else:
+        return [[]]
+
+def resta(A, B):
+    if len(A)==len(B) and len(A[0])==len(B[0]):
+        m = len(A)
+        n = len(B[0])
+
+        C = generaInicializada(m, n)
+        for i in range(m):
+            for j in range(n):
+                C[i][j] = A[i][j]-B[i][j]
+        return C
+    else:
+        return [[]]
 
 if __name__ == '__main__':
     system('cls')
 
-    m = int(input('¿Cuántas filas? '))
-    n = int(input('¿Cuántas columnas? '))
+    print('Para A')
+    mA = int(input('¿Cuántas filas? '))
+    nA = int(input('¿Cuántas columnas? '))
+    print('Para B')
+    mB = int(input('¿Cuántas filas? '))
+    nB = int(input('¿Cuántas columnas? '))
 
-    M = generaConRandrange(m, n)
-    MT = generaTranspuesta(M)
+    if mA!=mB or nA!=nB:
+        print('Error! No pueden sumarse ni restarse')
+    else:
 
-    print('\nM')
-    muestraMatriz(M)
-    print()
+        A = generaConRandrange(mA, nA, 0, 11)
+        B = generaConRandrange(mB, nB, 0, 11)
+        C = suma(A, B)
+        D = resta(A, B)
 
-    print('\nMT')
-    muestraMatriz(MT)
-    print()
+        print('\nA')
+        muestraMatriz(A)
+        print()
+
+        print('\nB')
+        muestraMatriz(B)
+        print()
+
+        print('\nA+B')
+        muestraMatriz(C)
+        print()
+
+        print('\nA-B')
+        muestraMatriz(D)
+        print()
