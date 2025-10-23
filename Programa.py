3530ba6cce5464b6b3056a7e6a1fd084f07a179f
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-10-22 20:33:37 -0600

    Genera Matriz con ceros

--- Programa.py
+++ Programa.py
@@ -1,19 +1,32 @@
 from os import system
 
-if __name__ == '__main__':
-    system('cls')
-
-    M = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-    print(M)
-    print()
-
+def muestraMatriz(M):
     m = len(M)
-    for i in range(m):
-        print( M[i] )
-    print()
-
     n = len(M[0])
+
     for i in range(m):
         for j in range(n):
             print(M[i][j], end='\t')
         print()
+
+def generaInicializada(m, n):
+    M = []
+    for i in range(m):
+        renglon = []
+        for j in range(n):
+            renglon.append(0)
+        M.append(renglon)
+        # print(M)
+    return M
+
+if __name__ == '__main__':
+    system('cls')
+
+    m = int(input('¿Cuántas filas? '))
+    n = int(input('¿Cuántas columnas? '))
+
+    M = generaInicializada(m, n)
+
+    print('\nM')
+    muestraMatriz(M)
+    print()
