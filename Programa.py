7526a9ee23703e88850e07fd67d493b26411101d
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-10-22 19:11:47 -0600

    Comprencion de lista (ceros)

--- Programa.py
+++ Programa.py
@@ -1,16 +1,7 @@
 from os import system
-from random import randrange
 
-def generaListaRandrage(n, ini, fin):
-    lista = []   # lista vacia
-    if ini>fin:
-        aux = ini
-        ini=fin
-        fin=aux
-
-    for i in range(n):
-        valor = randrange(ini, fin)
-        lista.append( valor )
+def generaListaCeros(n):
+    lista = [0 for i in range(n)]
     return lista
 
 def muestraLista(L):
@@ -22,12 +13,9 @@ if __name__ == '__main__':
     system('cls')
 
     n = int(input('¿Cuántos datos? '))
-    ini = int(input('Ingresa inicio de rango '))
-    fin = int(input('Ingresa fin de rango '))
 
-    lista = generaListaRandrage(n, ini, fin)
-    print()
+    ceros = generaListaCeros(n)
 
-    print(f'lista = {lista}')
-    muestraLista(lista)
+    print(f'ceros = {ceros}')
+    muestraLista(ceros)
     print()
