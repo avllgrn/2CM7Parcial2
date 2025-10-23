7db55eeb0a631a1ba6866ca5fe8b065172366e9c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-10-22 20:00:06 -0600

    En lista: busca, cuenta, acumula, promedia

--- Programa.py
+++ Programa.py
@@ -15,16 +15,61 @@ def muestraLista(L):
     for i in range(n):
         print(f'[{i}] = {L[i]}')
 
+def buscaDato(L, dato):
+    n = len(L)
+    posDato = None
+    for i in range(n):
+        if dato == L[i]:
+            posDato = i
+            break
+    return posDato
+
+def cuentaDato(L, dato):
+    veces = 0
+    n = len(L)
+    for i in range(n):
+        if dato == L[i]:
+            veces += 1
+    return veces
+
+def acumulaDatos(L):
+    n=len(L)
+    s = 0
+    for i in range(n):
+        s = s + L[i]
+    return s
+
+def promediaDatos(L):
+    if len(L)!=0:
+        return acumulaDatos(L)/len(L)
+    else:
+        return None
+
 if __name__ == '__main__':
     system('cls')
 
-    n = int(input('¿Cuántos datos? '))
-    ini = int(input('Ingresa inicio de rango '))
-    fin = int(input('Ingresa fin de rango '))
+    n = int(input('Cuántos datos? '))
 
-    lista = generaListaRandrage(n, ini, fin)
+    L = generaListaRandrage(n, 0, 10)
+    print()
+    muestraLista(L)
+
+    dato = int(input('¿Qué dato buscas? '))
+    posDato = buscaDato(L,dato)
+    if posDato!=None:
+        print(f'{dato} está en la posición {posDato}')
+    else:
+        print(f'{dato} NO está en la lista')
     print()
 
-    print(f'lista = {lista}')
-    muestraLista(lista)
+    veces = cuentaDato(L,dato)
+    print(f'{dato} está {veces} veces')
+    print()
+
+    s = acumulaDatos(L)
+    print(f'Los datos en L acumulan {s}')
+    print()
+
+    p = promediaDatos(L)
+    print(f'Los datos en L promedian {p}')
     print()
